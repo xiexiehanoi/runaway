0d3bf682659ceca5a55718a4474acbdfc75ba432
cyanO94 <dhdnjscks@gmail.com>
2024-01-28 00:47:07 +0900

    위경도 받는 함수 추가

--- src/main/reactjs/src/Running.js
+++ src/main/reactjs/src/Running.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+function getGeoLocation() {
+    if (window.Android) {
+        window.Android.getGeoLocation();
+    }
+}
 
 function Running() {
+    const [geoLocationList, setGeoLocationList] = useState([]);
+
+    useEffect(() => {
+        const handleGeoLocationCallback = (e) => {
+            let obj = JSON.parse(e.detail)
+            let latitude = obj["latitude"]
+            let longitude = obj["longitude"]
+            setGeoLocationList(prevList => [...prevList, { latitude, longitude }]);
+        };
+
+        window.addEventListener("onGeoLocationCallback", handleGeoLocationCallback);
+
+        return () => {
+            window.removeEventListener("onGeoLocationCallback", handleGeoLocationCallback);
+        };
+    }, []);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            getGeoLocation();
+            console.log("Function called every 1 second");
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, []);
+
+
     return (
         <div>
-            SOSO
+            Running
+            <ul>
+                {geoLocationList.map((location, index) => (
+                    <li key={index}>
+                        위도: {location.latitude}, 경도: {location.longitude}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
