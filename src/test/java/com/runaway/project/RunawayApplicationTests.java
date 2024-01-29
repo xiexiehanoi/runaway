458fe19a4938d625b590394d132946c12b5aebca
jongbin <garon6484@gmail.com>
2024-01-29 16:51:19 +0900

    fix: contextLoads() 빌드 오류 조치_1

--- src/test/java/com/runaway/project/RunawayApplicationTests.java
+++ src/test/java/com/runaway/project/RunawayApplicationTests.java
@@ -3,7 +3,7 @@ package com.runaway.project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RunawayApplicationTests.class)
 class RunawayApplicationTests {
 
 	@Test
