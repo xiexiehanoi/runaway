ed8e823bdd3b39d52c33fa8fc382fa6f1868ac33
xiexiehanoi <ksw593vn@gmail.com>
2024-01-29 16:02:42 +0900

    [naverConfig 수정]

--- src/main/java/com/runaway/project/naver/storage/NaverConfig.java
+++ src/main/java/com/runaway/project/naver/storage/NaverConfig.java
@@ -1,16 +1,13 @@
 package com.runaway.project.naver.storage;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import lombok.Data;
 
 @Configuration
 @Data
-@PropertySource("classpath:./application.yml")
-// 프로퍼티 파일에서 선언된 값 중에서
-// ncp.* 이름으로 된 프로퍼티 값을 받을 필드를 선언한다.
 @ConfigurationProperties(prefix = "ncp")
 public class NaverConfig {
 	private String endPoint;
