10a43e1f0e36e9061d5378deed6c826bdd619f96
tjftjrgus777 <tjftjrgus123@gmail.com>
2024-01-31 11:01:26 +0900

    develop 배포 에러 확인용

--- src/main/reactjs/src/components/map/Map.js
+++ src/main/reactjs/src/components/map/Map.js
@@ -1,46 +1,38 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Map = ({ locations }) => {
     const [map, setMap] = useState(null); // 지도 인스턴스를 저장할 상태
-    const [polyline, setPolyline] = useState(null); // 폴리라인을 저장할 상태
-
 
     useEffect(() => {
+        const script = document.createElement('script');
+        script.async = true;
+        script.src = 'https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=38q1h3iyts';
+        document.head.appendChild(script);
 
-            const script = document.createElement('script');
-            script.async = true;
-            script.src = 'https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=38q1h3iyts';
-            document.head.appendChild(script);
-
-            script.onload = () => {
-                const mapOptions = {
-
-                    zoomControl: true, // 확대/축소 컨트롤 활성화
-                };
-
-                const newMap = new window.naver.maps.Map('map', mapOptions);
-
-                setMap(newMap); // 생성된 지도 인스턴스 저장
+        script.onload = () => {
+            const mapOptions = {
+                zoomControl: true, // 확대/축소 컨트롤 활성화
             };
 
+            const newMap = new window.naver.maps.Map('map', mapOptions);
+
+            setMap(newMap); // 생성된 지도 인스턴스 저장
+        };
     }, []);
 
     useEffect(() => {
         if (!map) return;
 
+        const initialPath = locations.map(loc => new window.naver.maps.LatLng(loc.latitude, loc.longitude));
+        new window.naver.maps.Polyline({
+            path: initialPath,
+            strokeColor: '#5347AA',
+            strokeOpacity: 0.8,
+            strokeWeight: 3,
+            map: map
+        });
 
-            const initialPath = locations.map(loc => new window.naver.maps.LatLng(loc.latitude, loc.longitude));
-            const newPolyline = new window.naver.maps.Polyline({
-                path: initialPath,
-                strokeColor: '#5347AA',
-                strokeOpacity: 0.8,
-                strokeWeight: 3,
-                map: map
-            });
-
-            setPolyline(newPolyline);
-
-    }, [map, locations]);
+    }, [locations]);
 
     return <div id="map" style={{ width: '100%', height: '400px' }}></div>;
 };
