0a82fba6a22f9c6f6885765ace2db207f86f710a
GaleRhee <galerhee@gmail.com>
2024-01-31 15:57:38 +0900

    [rebase]

---
+++ src/main/reactjs/src/components/Webcam/WebCam.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WebCam = () => {
+    return (
+        <div>
+            <h4 className='alert alert-info'>Webcam</h4>
+        </div>
+    );
+};
+
+export default WebCam;
