b62f4308a876939ed81696ee82fad8ef18f9bbb7
jongbin <garon6484@gmail.com>
2024-02-01 13:56:47 +0900

    fix: Navbar.js 파일 참조 제거

--- src/main/reactjs/src/router/RouterMain.js
+++ src/main/reactjs/src/router/RouterMain.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
 import Running from '../components/running/Running';
 import WebCam from '../components/Webcam/WebCam';
 import { Route, Routes } from 'react-router-dom';
