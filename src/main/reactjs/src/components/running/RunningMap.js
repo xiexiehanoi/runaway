af84b163b955228fa0b9fceb8ee49268699f0a2a
cyanO94 <dhdnjscks@gmail.com>
2024-02-01 16:44:00 +0900

    배포쪽 에러 체크

--- src/main/reactjs/src/components/running/RunningMap.js
+++ src/main/reactjs/src/components/running/RunningMap.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 
-import { NaverMap, Polyline, useNavermaps } from "react-naver-maps";
-import { Container as MapDiv } from 'react-naver-maps'
+import { NaverMap, Polyline,  } from "react-naver-maps";
+import { Container as MapDiv, useNavermaps } from 'react-naver-maps'
 
 function RunningMap({path}) {
     const naverMap = useNavermaps(null)
-    //const [polylinePath, setPolylinePath] = useState([])
     const polylinePath = path.map(loc => new window.naver.maps.LatLng(loc.latitude, loc.longitude));
     const [map, setMap] = useState(null)
 
@@ -13,14 +12,13 @@ function RunningMap({path}) {
         <MapDiv
             style={{
                 width: '100%',
-                height: '600px',
+                height: '200px',
             }}
         >
             <NaverMap
                 defaultCenter={ 
                     new naverMap.LatLng(37.359924641705476, 127.1148204803467)
                 }
-                center={polylinePath[path.length -  1]}
                 ref={setMap}
             > 
                 <Polyline
