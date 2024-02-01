95933364b42d730c81eb8c43c8841edbc6cb9b23
GaleRhee <galerhee@gmail.com>
2024-02-01 14:04:02 +0900

    [commit] : webcam-react 추가

--- src/main/java/com/runaway/project/repository/WebCamDaoInter.java
+++
@@ -1,8 +0,0 @@
-package com.runaway.project.repository;
-
-import com.runaway.project.dto.WebCamDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WebCamDaoInter extends JpaRepository<WebCamDto, Long> {
-
-}
