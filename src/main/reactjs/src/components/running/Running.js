4c7a8076aca975edb669a3abfed6d4aaf1b1be7c
tjftjrgus777 <tjftjrgus123@gmail.com>
2024-02-02 16:03:09 +0900

    지도 시작,멈춤 버튼 타이머 연결 리셋버튼 생성

--- src/main/reactjs/src/components/running/Running.js
+++ src/main/reactjs/src/components/running/Running.js
@@ -1,18 +1,18 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import RunningMap from "./RunningMap";
 
 const MockDataList = [
-    {latitude: 37.359924641705476, longitude: 127.1148204803467},
-    {latitude: 37.36343797188166, longitude: 127.11486339569092},
-    {latitude: 37.368520071054576, longitude: 127.11473464965819},
-    {latitude: 37.3685882848096, longitude: 127.1088123321533},
-    {latitude: 37.37295383612657, longitude: 127.10876941680907},
-    {latitude: 37.38001321351567, longitude: 127.11851119995116},
-    {latitude: 37.378546827477855, longitude: 127.11984157562254},
-    {latitude: 37.376637072444105, longitude: 127.12052822113036},
-    {latitude: 37.37530703574853, longitude: 127.12190151214598},
-    {latitude: 37.371657839593894, longitude: 127.11645126342773},
-    {latitude: 37.36855417793982, longitude: 127.1207857131958},
+    { latitude: 37.359924641705476, longitude: 127.1148204803467 },
+    { latitude: 37.36343797188166, longitude: 127.11486339569092 },
+    { latitude: 37.368520071054576, longitude: 127.11473464965819 },
+    { latitude: 37.3685882848096, longitude: 127.1088123321533 },
+    { latitude: 37.37295383612657, longitude: 127.10876941680907 },
+    { latitude: 37.38001321351567, longitude: 127.11851119995116 },
+    { latitude: 37.378546827477855, longitude: 127.11984157562254 },
+    { latitude: 37.376637072444105, longitude: 127.12052822113036 },
+    { latitude: 37.37530703574853, longitude: 127.12190151214598 },
+    { latitude: 37.371657839593894, longitude: 127.11645126342773 },
+    { latitude: 37.36855417793982, longitude: 127.1207857131958 },
 ];
 
 function generateMockData(count) {
@@ -28,10 +28,30 @@ function generateMockData(count) {
 function Running() {
     const [geoLocationList, setGeoLocationList] = useState([])
     const [initialLocation, setInitialLocation] = useState({})
+    const [timer, setTimer] = useState(0); // 타이머를 위한 상태
+    const [isRunning, setIsRunning] = useState(false); // 타이머가 실행 중인지 확인하는 상태
+    const [intervalId, setIntervalId] = useState(null); // setInterval의 ID를 저장
+
+
+    // 초 단위의 타이머 값을 00:00 형식으로 변환하는 함수
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / 60).toString().padStart(2, '0');
+        const seconds = (time % 60).toString().padStart(2, '0');
+        return `${minutes}:${seconds}`;
+    };
 
     const startRun = () => {
         console.log("Function startRun");
         setGeoLocationList([])
+
+        if (!isRunning) {
+            const id = setInterval(() => {
+                setTimer(prevTimer => prevTimer + 1); // 1초마다 타이머를 업데이트
+            }, 1000);
+            setIntervalId(id);
+            setIsRunning(true);
+        }
+
 
         if (window.Android) {
             window.Android.startRun();
@@ -41,6 +61,13 @@ function Running() {
     const stopRun = () => {
         console.log("Function stopRun");
 
+
+        if (isRunning) {
+            clearInterval(intervalId); // 타이머 멈춤
+            setIsRunning(false);
+        }
+
+
         if (window.Android) {
             window.Android.stopRun();
         }
@@ -49,23 +76,23 @@ function Running() {
     function onNavigatorCallback(pos) {
         const latitude = pos.coords.latitude;
         const longitude = pos.coords.longitude;
-        
+
         setInitialLocation({ latitude, longitude })
     }
-    
+
 
     useEffect(() => {
         if (navigator.geolocation) {
-            navigator.permissions.query( { name: "geolocation" })
-            .then(function (result) {
-                console.log(result)
-                if (result.state === "granted") {
-                    navigator.geolocation.getCurrentPosition(onNavigatorCallback, null, null);
-                }
-                else if (result.state === "prompt") {
-                    navigator.geolocation.getCurrentPosition(onNavigatorCallback, null, null);
-                }
-            })
+            navigator.permissions.query({ name: "geolocation" })
+                .then(function (result) {
+                    console.log(result)
+                    if (result.state === "granted") {
+                        navigator.geolocation.getCurrentPosition(onNavigatorCallback, null, null);
+                    }
+                    else if (result.state === "prompt") {
+                        navigator.geolocation.getCurrentPosition(onNavigatorCallback, null, null);
+                    }
+                })
         }
         else {
             console.log("Geolocation is not supported by this browser.");
@@ -102,6 +129,8 @@ function Running() {
         addMockDataWithDelay();
     };
 
+
+
     return (
         <div>
             Running
@@ -111,9 +140,11 @@ function Running() {
             <button onClick={handleMockDataClick}>Mock Data</button>
 
             <div>
-                <RunningMap path={geoLocationList} initialLocation={initialLocation}/>      
+                <RunningMap path={geoLocationList} initialLocation={initialLocation} />
             </div>
-
+            <div>
+                시간: {formatTime(timer)}
+            </div>
         </div>
     );
 }
