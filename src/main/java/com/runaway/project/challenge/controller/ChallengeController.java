0e021d6fa387c01d57b2e99abe6a4c2036c5a454
KIMINSIK <insikkim2439@gmail.com>
2024-02-05 12:07:51 +0900

    1.challengecontroller에 /runningchallenge/list 매핑 작성 2.runningdao에 jpa로 running 테이블 출력해주는 구문 작성 3.러닝 챌린지,맨몸운동 챌린지 리액트에 출력

--- src/main/java/com/runaway/project/challenge/controller/ChallengeController.java
+++ src/main/java/com/runaway/project/challenge/controller/ChallengeController.java
@@ -1,8 +1,10 @@
 package com.runaway.project.challenge.controller;
 
+import com.runaway.project.challenge.dao.RunningDao;
 import com.runaway.project.challenge.dto.ExerciseDto;
 import com.runaway.project.challenge.dao.ExerciseDao;
 
+import com.runaway.project.challenge.dto.RunningDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChallengeController {
     private final ExerciseDao exerciseDao;
+    private final RunningDao runningDao;
 
     @GetMapping("/exercisechallenge/list")
     public List<ExerciseDto> list()
@@ -20,5 +23,11 @@ public class ChallengeController {
         System.out.println("list>>");
 
         return exerciseDao.getAllexercise();
+    }
+
+    @GetMapping("/runningchallenge/list")
+    public List<RunningDto> list2()
+    {
+        return runningDao.getAllrunning();
     }
 }
