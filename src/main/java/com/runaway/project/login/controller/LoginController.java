fb0675363042f18a1d852c9081c5ddadf6a88aee
cyanO94 <dhdnjscks@gmail.com>
2024-02-06 17:37:53 +0900

    머지 누락

--- src/main/java/com/runaway/project/login/controller/LoginController.java
+++ src/main/java/com/runaway/project/login/controller/LoginController.java
@@ -7,20 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @ResponseBody
 public class LoginController {
-
-    @GetMapping("/")
-    public String mainPage() {
-
-        return "main";
-    }
     @GetMapping("/my")
     public String myPage() {
 
         return "my";
-    }
-
-    @GetMapping("/login")
-    public String loginPage(){
-        return "LoginPage";
     }
 }
