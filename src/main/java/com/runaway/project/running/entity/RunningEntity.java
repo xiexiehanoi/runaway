917004695b70c12d1b9367142448686136c25ee6
tjftjrgus777 <tjftjrgus123@gmail.com>
2024-02-07 16:53:12 +0900

    러닝 db update

--- src/main/java/com/runaway/project/running/entity/RunningEntity.java
+++ src/main/java/com/runaway/project/running/entity/RunningEntity.java
@@ -1,11 +1,13 @@
 package com.runaway.project.running.entity;
 
+import com.runaway.project.login.dto.UserDto;
 import com.runaway.project.running.dto.LocationDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Entity
@@ -17,18 +19,29 @@ public class RunningEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "running_id")
-    private Long idx;
+    private Long runIdx;
+
+    //private Long userIdx;
+
+    @ManyToOne
+    @JoinColumn(name = "user_idx", nullable = false)
+    private UserDto user; // 사용자 엔티티와의 관계를 나타내는 필드
+
+    private Timestamp runningDateTime;
 
     @Column(nullable = true)
     private double distance;
 
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "running_id")
-    private List<LocationEntity> path; ;
+    private double averagePace;
 
     @Column(nullable = true, length = 100)
     private String runningTime;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "running_id")
+    private List<LocationEntity> path;
+
+    private int calorie;
 
 
 }
