400ccf9848c4ee2e92e9da1f8144917f5e9ae960
tjftjrgus777 <tjftjrgus123@gmail.com>
2024-02-08 16:05:07 +0900

    러닝 기록 db에 저장완료

--- src/main/java/com/runaway/project/running/service/RunningService.java
+++ src/main/java/com/runaway/project/running/service/RunningService.java
@@ -4,21 +4,38 @@ package com.runaway.project.running.service;
 import com.runaway.project.running.dto.RunningDto;
 import com.runaway.project.running.entity.RunningEntity;
 import com.runaway.project.running.repository.RunningRepository;
+import com.runaway.project.user.entity.User;
+import com.runaway.project.user.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class RunningService {
     private final RunningRepository runningRepository;
+    private final UserRepository userRepository;
     private final ModelMapper modelMapper;
 
-    public void saveRunningRecord(RunningDto runningDto) {
+    public User findById(Long userIdx) {
+        Optional<User> OptionalUserEntity=userRepository.findById(userIdx);
+        if(OptionalUserEntity.isPresent()){
+            return OptionalUserEntity.get();
+        }else {
+            throw new EntityNotFoundException("User not found for id: " + userIdx);
+        }
+    }
+
+    public void saveRunningRecord(RunningDto runningDto, User userEntity) {
 
         RunningEntity runningEntity=modelMapper.map(runningDto, RunningEntity.class);
+        runningEntity.setUser(userEntity);
         runningRepository.save(runningEntity);
 
 
     }
+
 }
