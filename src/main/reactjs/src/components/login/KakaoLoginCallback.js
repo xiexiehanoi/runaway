66909236446b0fca020936df2ad4c380f40afd90
cyanO94 <dhdnjscks@gmail.com>
2024-02-08 12:07:56 +0900

    워닝 해결용

--- src/main/reactjs/src/components/login/KakaoLoginCallback.js
+++ src/main/reactjs/src/components/login/KakaoLoginCallback.js
@@ -1,21 +1,22 @@
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
 
 const KakaoLoginCallback = () => {
-    const navigate = useNavigate();
-    const code = new URL(window.location.href).searchParams.get('code')
+    const navigate = useRef(useNavigate());
 
     useEffect(() => {
         (async () => {
             try {
+                const code = new URL(window.location.href).searchParams.get('code')
+
                 const res = await axios.get(`https://localhost:9002/api/login/oauth2/token?code=${code}`);
                 const token = res.headers.authorization;
                 window.localStorage.setItem('token', token);
-                navigate(`/`);
+                navigate.current(`/`);
             } catch (e) {
                 console.error(e);
-                navigate('/');
+                navigate.current('/');
             }
         })();
     }, []);
