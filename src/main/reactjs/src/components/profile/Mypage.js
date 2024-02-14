76001af48929dd104dde25f4cfe194d49db7df99
tjftjrgus777 <tjftjrgus123@gmail.com>
2024-02-14 20:46:34 +0900

    프로필 러닝기록 보여주기

--- src/main/reactjs/src/components/profile/Mypage.js
+++ src/main/reactjs/src/components/profile/Mypage.js
@@ -3,87 +3,61 @@ import React, { useEffect, useRef, useState } from 'react';
 import { Container as MapDiv, NaverMap, Polyline  ,useNavermaps } from 'react-naver-maps'
 
 const Mypage = () => {
-    // 위치 데이터 상태 관리
-    const [paths, setPaths] = useState([]);
-    const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+  const [paths, setPaths] = useState([]);
+  const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
-    useEffect(() => {
-        //서버에서 위치 데이터 가져오기
+  useEffect(() => {
+      axios.get(`${BACKEND_URL}/api/profile/map/path`, {
+          params: { userId: 20 }
+      })
+      .then(function (response) {
+          const newPaths = response.data.map(item => item.path);
+          setPaths(prevPaths => [...prevPaths, ...newPaths]);
+      })
+      .catch(function (error) {
+          console.log(error);
+      });
+  }, []);
 
-        axios.get(`${BACKEND_URL}/api/profile/map/path`, {
-            params: {
-                userId: 20
-            }
-        })
-            .then(function (response) {
-                // 기존 상태에 새로운 paths 배열을 추가하는 로직
-              
-                const newPaths = response.data.map(item => item.path);
-                setPaths(prevPaths => [...prevPaths, ...newPaths]);
-
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-            .finally(function () {
-
-            });
-
-    }, [])
-
-    
-    return (
-        <MapDiv
-            style={{
-                width: '100%',
-                height: '200px',
-            }}
-        >
-            <MultipleMapsComponent paths={paths}/>      
-        </MapDiv>   
-    )
+  return (
+      <div style={{ width: '100%', height: '100px' }}>
+          {paths.map((path, index) => (
+            <MapDiv>
+              <SingleMapComponent key={index} path={path} id={`map-${index}`} />
+              </MapDiv>
+          ))}
+      </div>   
+  );
 };
 
-function MultipleMapsComponent({ paths }) {
-    return (
-      <div>
-        {paths.map((path, index) => (
-          <SingleMapComponent key={index} path={path} />
-        ))}
+function SingleMapComponent({ path, id }) {
+  const navermaps = useNavermaps(); // 이 함수의 구체적인 사용법은 라이브러리 문서 참조
+  const mapRef = useRef(null);
+  const initialCenter = path.length > 0 ? path[0] : null;
+  const polylinePath = path.map(item => new navermaps.LatLng(item.latitude, item.longitude));
+
+  useEffect(() => {
+    if (initialCenter && mapRef.current) {
+      mapRef.current.setCenter(new navermaps.LatLng(initialCenter.latitude, initialCenter.longitude));
+    }
+  }, [initialCenter, mapRef]);
+
+  return (
+      <div style={{ width: '100%', height: '100px', marginBottom: '20px' }} id={id}>
+          <NaverMap
+            ref={mapRef}
+            style={{ width: '100%', height: '100px' }}
+            mapDivId={id} // `mapDivId` prop 사용하여 고유한 컨테이너에 지도를 렌더링
+          >
+              {path.length > 0 && (
+                  <Polyline
+                    path={polylinePath}
+                    strokeColor="#5347AA"
+                  />
+              )}
+          </NaverMap>
       </div>
-    );
-  }
-  
-  function SingleMapComponent({ path }) {
-    const navermaps = useNavermaps(); 
-    const mapRef = useRef(null);
-    
-    // 경로의 첫 번째 위치를 지도의 초기 중심으로 사용
-    const initialCenter = path.length > 0 ? path[0] : null;
-    const polylinePath = path.map(item => new window.naver.maps.LatLng(item.latitude, item.longitude));
-  
-    useEffect(() => {
-      if (initialCenter && mapRef.current) {
-        mapRef.current.setCenter(new window.naver.maps.LatLng(initialCenter.latitude, initialCenter.longitude));
-      }
-    }, [initialCenter, mapRef]);
-  
-    console.log(polylinePath);
-    return (
-      <div style={{ width: '100%', height: '400px', marginBottom: '20px' }}>
-        <NaverMap
-          ref={mapRef}
-          style={{ width: '100%', height: '100%' }}
-        >
-          {path.length > 0 && (
-            <Polyline
-              path={polylinePath}
-              strokeColor="#5347AA"
-            />
-          )}
-        </NaverMap>
-      </div>
-    );
-  }
+  );
+}
 
 export default Mypage;
