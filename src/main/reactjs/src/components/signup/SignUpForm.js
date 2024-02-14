31ce4d2d2f22a1684fa532fccb1d692f7c8a5232
jongbin <garon6484@gmail.com>
2024-02-14 21:12:18 +0900

    회원가입 폼 완성

--- src/main/reactjs/src/components/signup/SignUpForm.js
+++ src/main/reactjs/src/components/signup/SignUpForm.js
@@ -1,260 +1,308 @@
-import React, { useState }  from 'react';
-import axios from "axios";
+import React, {useState} from 'react';
 import {useNavigate} from "react-router-dom";
+import "../../CSS/SignUp.css"
 
 const SignUpForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
-  const [username, setUsername] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [birthdate, setBirthdate] = useState("");
-  const [phone, setPhone] = useState("");
-  const [gender, setGender] = useState("");
-  const [height, setHeight] = useState(0);
-  const [weight, setWeight] = useState(0);
-  const [verificationCode, setVerificationCode] = useState("");
-
-  // 오류메세지 상태 저장
-  const [emailMessage, setEmailMessage] = useState("");
-  const [passwordMessage, setPasswordMessage] = useState("");
-  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState("");
-  const [phoneMessage, setPhoneMessage] = useState("");
-  const [birthMessage, setBirthMessage] = useState("");
-  const [heightMessage, setHeightMessage] = useState("");
-  const [weightMessage, setWeightMessage] = useState("");
-  const [codeMessage, setCodeMessage] = useState("");
-
-  // 유효성 검사
-  const [isEmail, setIsEmail] = useState(false);
-  const [isPassword, setIsPassword] = useState(false);
-  const [isPasswordConfirm, setIsPasswordConfirm] = useState(false);
-  const [isUsername, setIsUsername] = useState(false);
-  const [isPhone, setIsPhone] = useState(false);
-  const [isBirth, setIsBirth] = useState(false);
-  const [isWeight, setIsWeight] = useState(false);
-  const [isHeight, setIsHeight] = useState(false);
-
+  const BASE_URL = process.env.REACT_APP_BACKEND_URL;
   const navi = useNavigate();
 
-  const BASE_URL = process.env.REACT_APP_BACKEND_URL;
-  const onChangeEmail = (e) => {
-    const currentEmail = e.target.value;
-    setEmail(currentEmail);
-    const emailRegExp =
-      /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
+  const [userInput, setUserInput] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    username: '',
+    nickname: '',
+    birthdate: '',
+    gender: '',
+    year: '',
+    month: '',
+    day: '',
+    height: 0,
+    weight: 0,
+  });
 
-    if (!emailRegExp.test(currentEmail)) {
-      setEmailMessage("이메일의 형식이 올바르지 않습니다!");
-      setIsEmail(false);
-    } else {
-      setEmailMessage("사용 가능한 이메일 입니다.");
-      setIsEmail(true);
-    }
+  const { email, password,  passwordConfirm, username, nickname, gender, year, month, day, height, weight } = userInput;  // 구조분해 할당하기
+
+  // 상수 데이터
+  // 년
+  const YEAR = [];
+
+  const nowYear = new Date().getFullYear();
+  for (let i = 1980; i <= nowYear; i++) {
+    YEAR.push(i);
+  }
+
+  // 월
+  const MONTH = [];
+
+  for (let i = 1; i <= 12; i++) {
+    let m = String(i).padStart(2, '0');
+    MONTH.push(m);
+  }
+
+  // 일
+  const DAY = [];
+  for (let i = 1; i <= 31; i++) {
+    let d = String(i).padStart(2, '0');
+    DAY.push(d);
+  }
+
+  const handleInput = e => {
+    const { name, value } = e.target;
+    setUserInput({ ...userInput, [name]: value });
   };
-  const onChangePassword = (e) => {
-    const currentPassword = e.target.value;
-    setPassword(currentPassword);
+
+  // 이메일 유효성 검사
+  const isEmail = email => {
+    const emailRegExp = /^[a-z0-9_+.-]+@([a-z0-9-]+\.)+[a-z0-9]{2,4}$/;
+    return emailRegExp.test(email);
+  };
+  const isEmailValid = isEmail(email);
+  const emailCheck = ( !isEmailValid && email.length > 0 ) ? 'emailCheck' : undefined;
+
+  // 패스워드 유효성 검사
+  const isPassword = password => {
     const passwordRegExp =
-      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
-    if (!passwordRegExp.test(currentPassword)) {
-      setPasswordMessage(
-        "숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!"
-      );
-      setIsPassword(false);
-    } else {
-      setPasswordMessage("안전한 비밀번호 입니다.");
-      setIsPassword(true);
-    }
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    return passwordRegExp.test(password);
   };
-  const onChangePasswordConfirm = (e) => {
-    const currentPasswordConfirm = e.target.value;
-    setPasswordConfirm(currentPasswordConfirm);
-    if (password !== currentPasswordConfirm) {
-      setPasswordConfirmMessage("비밀번호가 다릅니다.");
-      setIsPasswordConfirm(false);
-    } else {
-      setPasswordConfirmMessage("비밀번호 확인 완료");
-      setIsPasswordConfirm(true);
-    }
-  };
+  const isPasswordValid = isPassword(password);
+  const passwordCheck = ( !isPasswordValid && password.length > 0 ) ? 'passwordCheck' : undefined;
 
-  const onChangeUsername = (e) => {
-    const currentUsername = e.target.value;
-    setUsername(currentUsername);
-  };
 
-  const onChangeNickname = (e) => {
-    const currentNickname = e.target.value;
-    setNickname(currentNickname);
-  };
+  // 패스워드 재확인
+  const isPasswordConfirm = password === passwordConfirm;
+  const passwordConfirmCheck = ( !isPasswordConfirm && passwordConfirm.length > 0 ) ? 'passwordConfirmCheck' : undefined;
 
-  const onChangePhone = (e) => {
-    const currentPhone = e.target.value;
-    setPhone(currentPhone);
-  };
+  // 이름 입력여부 확인
+  const isUsername = Boolean(username);
 
-  const onChangeBirthdate = (e) => {
-    const currentBirthdate = e.target.value;
-    setBirthdate(currentBirthdate);
-  };
+  // 성별 체크여부 확인
+  const isGender = Boolean(gender);
+  
+  // 생년월일 입력여부 확인
+  const isBirthdate = Boolean(year && month && day);
+  
+  // 키 입력 여부 확인
+  const isHeight = Boolean(height);
 
-  const onChangeGender = (e) => {
-    const currentGender = e.target.value;
-    setGender(currentGender);
-  };
+  // 몸무게 입력 여부 확인
+  const isWeight = Boolean(weight);
 
-  const onChangeHeight = (e) => {
-    const currentHeight = e.target.value;
-    setHeight(currentHeight);
-  };
 
-  const onChangeWeight = (e) => {
-    const currentWeight = e.target.value;
-    setWeight(currentWeight);
-  };
-  // const handleDuplicateCheck = () => {
-  //   // 여기에서 아이디 중복 확인 로직을 추가합니다.
-  //   console.log('Checking duplicate for username:', username);
-  // };
+  // 개인정보 유효기간
+  // const isTimeValid = Boolean(time);
+  
+  const isAllValid =
+    isEmailValid &&
+    isPasswordValid &&
+    isPasswordConfirm &&
+    isUsername &&
+    isGender &&
+    isHeight &&
+    isWeight &&
+    isBirthdate;
 
-  const handleSendVerificationCode = () => {
-    // 여기에서 이메일 또는 전화번호로 인증번호를 전송하는 로직을 추가합니다.
-    console.log('Sending verification code to email or phone number:', email || phone);
-  };
+  const activeBtn = isAllValid ? 'undefined' : 'disabled';
 
-  const handleSubmit = (e) => {
+  // 통신
+  const checkSignUp = e => {
     e.preventDefault();
-
-    axios.post(`${BASE_URL}/api/user/sign-up`,
-      { email, password, username, nickname, phone, gender, birthdate, height, weight })
-      .then((res) => {
-      // 멤버 추가후 어디로 갈지??
-      navi("/home");
-    });
-    // 여기에서 서버로 회원가입 데이터를 전송하는 로직을 추가합니다.
-    console.log('Name:', username);
-    console.log('Username:', username);
-    console.log('Password:', password);
-    console.log('Email:', email);
-    console.log('Verification Code:', verificationCode);
+    console.log(userInput);
+    fetch(`${BASE_URL}/api/user/sign-up`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        username: username,
+        nickname: nickname,
+        birthdate: `${year}-${month}-${day}`,
+        gender: gender,
+        height: parseInt(height),
+        weight: parseInt(weight),
+      }),
+    })
+      .then(response => {
+        if (response.ok) {
+          alert('회원가입 성공');
+          navi("/login");
+        } else {
+          throw new Error('회원가입 실패');
+        }
+      })
+      .catch(error => alert(error.message));
   };
-
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <h3>회원 가입</h3>
-          <div className="form">
-            {/* 이메일 */}
-            <div className="form-el">
-              <label htmlFor="email">이메일</label> <br/>
+      <div className="signUp">
+        <h4>회원 가입</h4>
+        <form className="signUpBox">
+          {/* <div className="profileBox">
+            <label className="imgBoxLabel" htmlFor="profileImg">
+              {imageUrl ? (
+                <img className="labelImg" src={imageUrl} alt="uploadImg"/>
+              ) : null}
+              <div className="imgUploadBtn">
+                <i className="fa-sharp fa-solid fa-camera"/>
+              </div>
               <input
-                id="email"
-                name="email"
-                value={email}
-                onChange={onChangeEmail}
+                id="profileImg"
+                className="profileImgInput"
+                type="file"
+                name="imageUrl"
+                ref={imgRef}
+                onChange={onChangeImage}
               />
-              <p className="message">{emailMessage}</p>
-            </div>
+            </label>
+          </div> */}
 
-            {/* 비밀번호 */}
-            <div className="form-el">
-              <label htmlFor="password">비밀번호</label> <br/>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                value={password}
-                onChange={onChangePassword}
-              />
-              <p className="message">{passwordMessage}</p>
-            </div>
-
-            {/*비밀번호 확인*/}
-            <div className="form-el">
-              <label htmlFor="passwordConfirm">비밀번호 확인</label> <br/>
-              <input
-                type="password"
-                id="passwordConfirm"
-                name="passwordConfirm"
-                value={passwordConfirm}
-                onChange={onChangePasswordConfirm}
-              />
-              <p className="message">{passwordConfirmMessage}</p>
-            </div>
-
-            <div className="form-el">
-              <label htmlFor="name">이름</label> <br/>
-              <input id="username" name="username" value={username} onChange={onChangeUsername}/>
-              <p className="message">{}</p>
-            </div>
-
-            <div className="form-el">
-              <label htmlFor="name">닉네임</label> <br/>
-              <input id="nickname" name="nickname" value={nickname} onChange={onChangeNickname}/>
-              <p className="message">{}</p>
-            </div>
-
-
-            <div className="form-el">
-              <label htmlFor="phone">핸드폰</label> <br/>
-              <input id="phone" name="phone" value={phone} onChange={onChangePhone}/>
-              <p className="message">{}</p>
-            </div>
-
-            <div className="form-el">
-              <label htmlFor="gender">성별</label> <br/>
-              <select
-                id="gender"
-                name="gender"
-                value={gender}
-                onChange={onChangeGender}
-              >
-                <option value="">선택하세요</option>
-                <option value="남자">남자</option>
-                <option value="여자">여자</option>
+          {/* 이메일 비밀번호 입력 */}
+          <p className="userEmail title mustInput">이메일</p>
+          <input
+            onChange={handleInput}
+            className={`userInputEmail input ${emailCheck}`}
+            name="email"
+            type="text"
+            placeholder="이메일"
+            autoComplete="username"
+          />
+          {!isEmailValid && (
+            <p
+              className="inputCheck"
+              style={{display: email.length > 0 ? 'block' : 'none'}}
+            >
+              * 이메일 양식을 맞춰주세요!
+            </p>
+          )}
+          <hr />
+          <p className="userPassword title mustInput">비밀번호</p>
+          <input
+            onChange={handleInput}
+            className={`userInputPw input ${passwordCheck}`}
+            name="password"
+            type="password"
+            placeholder="비밀번호"
+            autoComplete="current-password"
+          />
+          {!isPasswordValid && (
+            <p
+              className="inputCheck"
+              style={{display: password.length > 0 ? 'block' : 'none'}}
+            >
+              * 비밀번호는 대소문자, 숫자, 특수문자 포함 8자리 이상 적어주세요!
+            </p>
+          )}
+          <input
+            onChange={handleInput}
+            className={`userInputPwConfirm input ${passwordConfirmCheck}`}
+            name="passwordConfirm"
+            type="password"
+            placeholder="비밀번호 확인"
+            autoComplete="current-password"
+          />
+          <hr />
+          {/* 이름 입력 */}
+          <p className="userName title mustInput">이름</p>
+          <input
+            onChange={handleInput}
+            className="userInputName input"
+            name="username"
+            type="text"
+            placeholder="이름을(를) 입력하세요"
+            autoComplete="username"
+          />
+          <hr />
+          {/* 닉네임 입력 */}
+          <p className="userNickname title mustInput">닉네임</p>
+          <input
+            onChange={handleInput}
+            className="userInputNickname input"
+            name="nickname"
+            type="text"
+            placeholder="닉네임을(를) 입력하세요"
+            autoComplete="username"
+          />
+          <hr />
+          {/* 성별 입력 */}
+          <p className="userGender title mustInput">성별</p>
+          <label className="userMale label">
+            <input
+              onChange={handleInput}
+              className="radio"
+              name="gender"
+              type="radio"
+              value="male"
+            />
+            <span className="text">남자</span>
+          </label>
+          <label className="userFemale label">
+            <input
+              onChange={handleInput}
+              className="radio"
+              name="gender"
+              type="radio"
+              value="female"
+            />
+            <span className="text">여자</span>
+          </label>
+          <hr />
+          {/* 생년월일 입력 */}
+          <div className="userBirthdate">
+            <p className="title mustInput">생년월일</p>
+            <div className="selectBox">
+              <select className="select" name="year" onChange={handleInput}>
+                <option value="" selected disabled hidden>연도</option>
+                {YEAR.map(y => {
+                  return <option key={y}>{y}</option>;
+                })}
+              </select>
+              <select className="select" name="month" onChange={handleInput}>
+                <option value="" selected disabled hidden>월</option>
+                {MONTH.map(m => {
+                  return <option key={m}>{m}</option>;
+                })}
+              </select>
+              <select className="select" name="day" onChange={handleInput}>
+                <option value="" selected disabled hidden>일</option>
+                {DAY.map(d => {
+                  return <option key={d}>{d}</option>;
+                })}
               </select>
             </div>
-
-            <div className="form-el">
-              <label htmlFor="birthdate">생년월일</label> <br/>
-              <input
-                id="birthdate"
-                name="birthdate"
-                value={birthdate}
-                onChange={onChangeBirthdate}
-              />
-              <p className="message">{}</p>
-            </div>
-
-            <div className="form-el">
-              <label htmlFor="height">키</label> <br/>
-              <input
-                id="height"
-                name="height"
-                value={height}
-                onChange={onChangeHeight}
-              />
-              <p className="message">{}</p>
-            </div>
-
-            <div className="form-el">
-              <label htmlFor="weight">몸무게</label> <br/>
-              <input
-                id="weight"
-                name="weight"
-                value={weight}
-                onChange={onChangeWeight}
-              />
-              <p className="message">{}</p>
-            </div>
-            <br/>
-            <br/>
-            <button type="submit">가입 완료</button>
           </div>
-        </div>
-      </form>
+          <hr/>
+          {/* 키 입력 */}
+          <p className="userHeight title mustInput">키</p>
+          <input
+            onChange={handleInput}
+            className="userInputHeight numInput"
+            name="height"
+            type="number"
+            placeholder="cm"
+          />
+          <hr />
+          {/* 몸무게 입력 */}
+          <p className="userWeight title mustInput">몸무게</p>
+          <input
+            onChange={handleInput}
+            className="userInputWeight numInput"
+            name="weight"
+            type="number"
+            placeholder="kg"
+          />
+
+          {/* 개인정보 유효기간 */}
+          <div className="agree">
+            <p className="name title">약관 동의</p>
+          </div>
+          <div className={`signupBtn ${activeBtn}`} onClick={checkSignUp}>
+            가입하기
+          </div>
+        </form>
+      </div>
     </>
   );
 };
