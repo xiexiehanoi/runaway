ecb7449ff0406e2be57fd429a8183c574a846d93
xiexiehanoi <ksw593vn@gmail.com>
2024-02-15 15:23:52 +0900

    [fix] : jwt통해 userid받아오기 성공

--- src/main/reactjs/src/components/challenge/ExerciseChallenge.js
+++ src/main/reactjs/src/components/challenge/ExerciseChallenge.js
@@ -6,7 +6,7 @@ const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 const ExerciseChallenge = () => {
     const [exerciseList,setExerciseList]=useState([]);
-
+    const [userId, setUserId] = useState(null);
 
     useEffect(()=>{
         const list = async()=>{
@@ -23,6 +23,29 @@ const ExerciseChallenge = () => {
 
     },[])
 
+    useEffect(() => {
+        const getUserId = async () => {
+            try {
+                const token = window.localStorage.getItem('token');
+                if (!token) {
+                    console.log("Token not found.");
+                    return;
+                }
+                
+                const response = await axios.get(`${BACKEND_URL}/api/login/me`, {
+                    headers: {
+                        Authorization: token
+                    }
+                });
+                setUserId(response.data.id);
+            } catch (error) {
+                console.error("Failed to fetch user ID:", error);
+            }
+        };
+
+        getUserId();
+    }, []);
+
     return (
         <div>
             <h5>총 {exerciseList.length}개의 맨몸운동 챌린지에 도전하세요!</h5>
@@ -30,7 +53,7 @@ const ExerciseChallenge = () => {
                 <tbody>
                     {
                         exerciseList.map((rowData,idx)=>
-                            <ExerciseChallengeRowItem key={idx} row={rowData} idx={idx}/>
+                            <ExerciseChallengeRowItem key={idx} row={rowData} userId={userId}/>
                         )
                     }
                 </tbody>
