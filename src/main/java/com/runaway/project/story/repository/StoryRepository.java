6076a8c3517b84ee85427a4e6ac360684d48a9c5
GaleRhee <galerhee@gmail.com>
2024-02-19 15:06:21 +0900

    [commit] : story add with user info

--- src/main/java/com/runaway/project/story/repository/StoryRepository.java
+++ src/main/java/com/runaway/project/story/repository/StoryRepository.java
@@ -1,7 +1,11 @@
 package com.runaway.project.story.repository;
 
-import com.runaway.project.story.dto.StoryDto;
+import com.runaway.project.story.entity.StoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface StoryRepository extends JpaRepository<StoryDto, Long> {
+public interface StoryRepository extends JpaRepository<StoryEntity, Long> {
+
+//    @Query("update story set storyWatchCount=storyWatchCount+1 where storyNum=#{storyNum}")
+//    public void updateStoryWatchCount(Long storyNum);
 }
