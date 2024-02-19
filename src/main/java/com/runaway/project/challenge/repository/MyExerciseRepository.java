5719953dad487d0c643243ea48e313277841ba89
재형 <>
2024-02-19 16:43:28 +0900

    challenge 중복 유효성검사

--- src/main/java/com/runaway/project/challenge/repository/MyExerciseRepository.java
+++ src/main/java/com/runaway/project/challenge/repository/MyExerciseRepository.java
@@ -2,6 +2,13 @@ package com.runaway.project.challenge.repository;
 
 import com.runaway.project.challenge.dto.MyExerciseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface MyExerciseRepository extends JpaRepository<MyExerciseDto,Long> {
+import java.time.LocalDate;
+import java.util.List;
+
+public interface MyExerciseRepository extends JpaRepository<MyExerciseDto, Long> {
+    @Query("SELECT me FROM MyExerciseDto me WHERE me.user.id = :userId AND me.exerciseChallengeDto.exercise_type = :exerciseType AND me.end_date > CURRENT_DATE")
+    List<MyExerciseDto> findAllActiveByUserIdAndExerciseType(@Param("userId") Long userId, @Param("exerciseType") String exerciseType);
 }
