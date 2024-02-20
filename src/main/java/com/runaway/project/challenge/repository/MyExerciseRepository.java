1b00ad7a744f55baf72373bb24eb2cd2137c18b3
KIMINSIK <insikkim2439@gmail.com>
2024-02-20 10:50:59 +0900

    mychallengelist 출력 완료

--- src/main/java/com/runaway/project/challenge/repository/MyExerciseRepository.java
+++ src/main/java/com/runaway/project/challenge/repository/MyExerciseRepository.java
@@ -16,6 +16,7 @@ import java.util.List;
 public interface MyExerciseRepository extends JpaRepository<MyExerciseDto, Long> {
     @Query("SELECT me FROM MyExerciseDto me WHERE me.user.id = :userId AND me.exerciseChallengeDto.exercise_type = :exerciseType AND me.end_date > CURRENT_DATE")
     List<MyExerciseDto> findAllActiveByUserIdAndExerciseType(@Param("userId") Long userId, @Param("exerciseType") String exerciseType);
+
     @Query("select mx from MyExerciseDto mx where mx.user.id=:userId order by mx.start_date DESC")
     List<MyExerciseDto> findByUserExerciseChallengeList(@Param("userId") Long userId);
 
