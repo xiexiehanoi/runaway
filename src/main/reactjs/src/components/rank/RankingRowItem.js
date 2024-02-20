21ec85d6f5aa11a84c27f74892d9bffe5c7f00aa
KIMINSIK <insikkim2439@gmail.com>
2024-02-20 18:03:39 +0900

    랭킹 css

--- src/main/reactjs/src/components/rank/RankingRowItem.js
+++ src/main/reactjs/src/components/rank/RankingRowItem.js
@@ -5,10 +5,10 @@ const RankingRowItem = (props) => {
 
     return (
         <tr key={row.id}>
-            <td className="number">{idx + 1}위</td>
+            <td className="number">{idx + 1}</td>
             <td className="name">{row.nickname}</td>
             <td className="points">{row.point}</td>
-          </tr>
+        </tr>
     );
 };
 
