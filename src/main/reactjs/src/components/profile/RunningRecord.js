011768d01913f882dcb14b9091b6a70d32a9200b
tjftjrgus777 <tjftjrgus123@gmail.com>
2024-02-20 17:28:49 +0900

    러닝기록 css 리스트 완성

--- src/main/reactjs/src/components/profile/RunningRecord.js
+++ src/main/reactjs/src/components/profile/RunningRecord.js
@@ -1,45 +1,68 @@
 import axios from 'axios';
 import React, { useEffect, useState, Fragment } from 'react';
 import { Link } from 'react-router-dom';
-//import './css/RunningRecord.css'
+import './css/RunningRecord.css'
+import defaultImage from './Img/default-img.jpg';
 function RunningRecord(props) {
     const [runningRecord, setRunningRecord] = useState([]);
     const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
     useEffect(() => {
         axios.get(`${BACKEND_URL}/api/profile/running/record`, {
-            params: { userId: 20 }
+          params: { userId: 20 }
         })
-            .then(function (response) {
-                setRunningRecord(response.data);
-                // Assuming that `response.data` is an array of records and `item.path` is defined
-            })
-            .catch(function (error) {
-                console.error(error);
-            });
-    }, []);
-
-
-
-    return (
-        <div style={{ width: '100%', fontFamily: 'sans-serif' }}>
-            <Link to={'/my'}>내정보</Link>
-            {runningRecord.map((item) => (
-                <Fragment key={item.runIdx}>
-                    <div style={{ cursor: 'pointer', marginBottom: '20px', border: '1px solid #ddd', borderRadius: '8px', padding: '10px' }}>
-                        <Link to={`/runningRecordDetail/${item.runIdx}`}>
-                            <h2>RunIdx {item.runIdx}</h2>
-                            <p>Date: {item.date}</p>
-                            <p>Distance: {item.distance} km</p>
-                            <p>Time: {item.runningTime}</p>
-                            <p>Calories: {item.calorie}</p>
-                        </Link>
-                    </div>
-                </Fragment>
-            ))}
+        .then(function (response) {
+          // 데이터 변환을 위한 함수
+          const getDayName = (date) => {
+            const dayNames = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+            return dayNames[new Date(date).getDay()];
+          };
+    
+          // 오전/오후 판단을 위한 함수
+          const getMeridiem = (time) => {
+            const hour = parseInt(time.split(':')[0], 10);
+            return hour < 12 ? '오전' : '오후';
+          };
+    
+          // 데이터를 변환하여 새로운 배열을 만듭니다.
+          const formattedData = response.data.map(item => ({
+            ...item,
+            dayName: getDayName(item.date), // 요일
+            meridiem: getMeridiem(item.time), // 오전/오후
+          }));
+    
+          setRunningRecord(formattedData);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+      }, []);
+    
+      return (
+        <div className="running-record-container">
+          {runningRecord.map((item, index) => (
+            <div key={index} className="record-item">
+              <img src={defaultImage} alt="Run" className="record-img" />
+              <div className="record-details">
+                <div className="record-full-date">
+                  {`${new Date(item.date).getFullYear()}년 ${new Date(item.date).getMonth() + 1}월 ${new Date(item.date).getDate()}일`}
+                </div>
+                <div className="record-date">
+                  {`${item.dayName} ${item.meridiem}`}
+                </div>
+                <div className="record-stats">
+                  <span className="record-distance">{item.distance} km</span>
+                  <span className="record-pace">{item.pace} /km</span>
+                  <span className="record-time">{item.runningTime}</span>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-    );
-};
+      );
+}
+    
+
 
 
 
