6fc50254f70328cc279566efaba4ce24c11a7a8b
재형 <>
2024-02-20 15:05:27 +0900

    challenge 진행상태

--- src/main/java/com/runaway/project/exercise/respository/ExerciseRepository.java
+++ src/main/java/com/runaway/project/exercise/respository/ExerciseRepository.java
@@ -3,7 +3,11 @@ package com.runaway.project.exercise.respository;
 
 import com.runaway.project.exercise.entity.ExerciseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 public interface ExerciseRepository extends JpaRepository<ExerciseEntity,Long> {
-
+    @Query("SELECT SUM(e.exerciseCount) FROM ExerciseEntity e WHERE e.date = :date AND e.exerciseType = :exerciseType AND e.user.id = :userId")
+    int sumExerciseCountByDateAndType(LocalDate date, String exerciseType, Long userId);
 }
