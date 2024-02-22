7773d7350709e8c3e4b1d59314602774bdc230a7
GaleRhee <galerhee@gmail.com>
2024-02-22 20:57:17 +0900

    [commit] : story list css + scroll bar start

--- src/main/reactjs/src/components/Webcam/VideoPlayer.js
+++ src/main/reactjs/src/components/Webcam/VideoPlayer.js
@@ -6,7 +6,7 @@ const VideoPlayer = ({ src }) => {
     // const aspectRatio = Math.min(width, height) / Math.max(width, height) * 100;
 
     return (
-        <video autoPlay loop src={src.src} type="video/mp4"
+        <video autoPlay loop src={src} type="video/mp4"
             style={{
                 objectFit: "cover",
                 width: '100%', height: '100%',
