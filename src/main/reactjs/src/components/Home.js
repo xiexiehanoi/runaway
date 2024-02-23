4b2cd812196f22d97eecbe40639ba43d36e10b8b
KIMINSIK <insikkim2439@gmail.com>
2024-02-23 19:37:04 +0900

    메인 css

--- src/main/reactjs/src/components/Home.js
+++ src/main/reactjs/src/components/Home.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
 import axios from "axios";
+import runawayimg from "../image/runaway.png";
+import situpImage from '../image/situp.png';
+import pushupImage from '../image/push-up.png';
+import squatImage from '../image/squats.png';
+import plusButton from '../image/plus-sign.png';
+import { Link } from 'react-router-dom';
 const Home = () => {
     var token = window.localStorage.getItem('token');
     const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
@@ -28,8 +34,46 @@ const Home = () => {
 
     return (
         <div>
-            홈화면
-            <Navbar/>
+            <div class="header-inscreen">
+                <span style={{fontFamily:'Anton' ,marginLeft: "8%" }}>Runaway</span>
+                
+            </div>   
+
+            <div className="exercise-container" >
+                <div className='exercise-content'  >
+                        <img src={plusButton} alt="running" className="exercise-image" />    
+                </div>
+                <span style={{fontFamily:'Anton',fontSize: '300%'}}><Link to="/challengemain">challenge</Link></span>
+            </div>
+
+            <div style={{ display: 'flex' }}>
+                <div className="exercise-container" >
+                    <img src={runawayimg}  alt="running" className='exercise-image' />
+                    <Link to="/running">Running</Link>
+                </div>
+                <div className="exercise-container" >
+                <img src={situpImage}  alt="situp" className='exercise-image' />  
+                <Link to={"/situp"}>Sit-UP</Link>
+                </div>
+
+            </div>
+
+            <div style={{ display: 'flex' }}>
+                <div className="exercise-container" >
+                <img src={pushupImage}  alt="pushup" className='exercise-image' />
+                <Link to={"/pushup"}>Push-UP</Link>
+                
+                
+                </div>
+                <div className="exercise-container" >
+                <img src={squatImage}  alt="squat" className='exercise-image' />
+                <Link to={"/squat"}>Squat</Link>
+                
+                </div>
+
+            </div>
+
+            {/* <Navbar/> */}
         </div>
     );
 };
