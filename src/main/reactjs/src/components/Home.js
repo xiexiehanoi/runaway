a9871d04150d4593cca13452baac2aff805b54e2
KIMINSIK <insikkim2439@gmail.com>
2024-02-26 14:16:16 +0900

    1.랭킹 포인트가 0보다 큰 튜플만 출력 2.메인 css 3.랭킹 123위 이미지 수정

--- src/main/reactjs/src/components/Home.js
+++ src/main/reactjs/src/components/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Navbar from './Navbar';
 import axios from "axios";
 import runawayimg from "../image/runaway.png";
-import situpImage from '../image/situp.png';
+import situpImage from '../image/sit-up.png';
 import pushupImage from '../image/push-up.png';
-import squatImage from '../image/squats.png';
-import plusButton from '../image/plus-sign.png';
+import squatImage from '../image/squat.png';
+import plusButton from '../image/plus-button.png';
 import { Link } from 'react-router-dom';
+import "../CSS/Main.css";
 const Home = () => {
     var token = window.localStorage.getItem('token');
     const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
@@ -38,41 +39,58 @@ const Home = () => {
                 <span style={{fontFamily:'Anton' ,marginLeft: "8%" }}>Runaway</span>
                 
             </div>   
+        
+        <div>
+            <Link to="/challengemain">
+                <button className='btn primaryButton-outset'
+                style={{ width:'100%',height:'80px',marginTop:'40%'}}>
+                    <span style={{color:'white' }}>challenge</span>
+                </button>
+            </Link>
+                    
+          
 
-            <div className="exercise-container" >
-                <div className='exercise-content'  >
-                        <img src={plusButton} alt="running" className="exercise-image" />    
+            <div style={{ display: 'flex',marginBottom:'-1%' }}>
+                
+                <div className="exercise-container" style={{ textAlign: 'center',marginRight:'5%',height:'100px' }}>
+                    <Link to={"/running"}>
+                        <img src={runawayimg}  alt="running" className='exercise-image' 
+                        style={{  width: '50px', height: '50px', display: 'block', margin: '0 auto' }}/>
+                        <span style={{color:'white' }}>Running</span>
+                    </Link>
                 </div>
-                <span style={{fontFamily:'Anton',fontSize: '300%'}}><Link to="/challengemain">challenge</Link></span>
+                
+                <div className="exercise-container" style={{ textAlign: 'center',height:'100px' }}>
+                    <Link to={"/situp"}>
+                        <img src={situpImage}  alt="situp" className='exercise-image' 
+                        style={{  width: '50px', height: '50px', display: 'block', margin: '0 auto' }}/>  
+                        <span style={{color:'white' }}>Sit-UP</span>
+                    </Link>
+                </div>
+               
+
             </div>
 
             <div style={{ display: 'flex' }}>
-                <div className="exercise-container" >
-                    <img src={runawayimg}  alt="running" className='exercise-image' />
-                    <Link to="/running">Running</Link>
-                </div>
-                <div className="exercise-container" >
-                <img src={situpImage}  alt="situp" className='exercise-image' />  
-                <Link to={"/situp"}>Sit-UP</Link>
+                <div className="exercise-container" style={{ textAlign: 'center',marginRight:'5%',height:'100px' }}>
+                    <Link to={"/pushup"}>
+                        <img src={pushupImage}  alt="pushup" className='exercise-image' 
+                        style={{ width: '50px', height: '50px', display: 'block', margin: '0 auto' }}/>
+                        <span style={{color:'white' }}>Push-UP</span>
+                    </Link>
                 </div>
 
-            </div>
 
-            <div style={{ display: 'flex' }}>
-                <div className="exercise-container" >
-                <img src={pushupImage}  alt="pushup" className='exercise-image' />
-                <Link to={"/pushup"}>Push-UP</Link>
-                
-                
-                </div>
-                <div className="exercise-container" >
-                <img src={squatImage}  alt="squat" className='exercise-image' />
-                <Link to={"/squat"}>Squat</Link>
-                
+                <div className="exercise-container" style={{ textAlign: 'center',height:'100px' }}>
+                    <Link to={"/squat"}>
+                        <img src={squatImage}  alt="squat" className='exercise-image' 
+                        style={{ width: '50px', height: '50px', display: 'block', margin: '0 auto' }}/>
+                        <span style={{color:'white' }}>Squat</span>
+                    </Link>
                 </div>
 
             </div>
-
+        </div>
             {/* <Navbar/> */}
         </div>
     );
