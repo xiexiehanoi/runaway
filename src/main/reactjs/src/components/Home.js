6f88ec97f3864239f9031a22dd199ab07c3ddf2e
KIMINSIK <insikkim2439@gmail.com>
2024-02-27 18:49:20 +0900

    1.package json 추가 2.메인화면에 슬라이드 추가

--- src/main/reactjs/src/components/Home.js
+++ src/main/reactjs/src/components/Home.js
@@ -2,13 +2,41 @@ import React from 'react';
 
 import axios from "axios";
 import runawayimg from "../image/runaway.png";
-//import situpImage from '../image/sit-up.png';
+import situpImage from '../image/sit-up.png';
 import pushupImage from '../image/push-up.png';
-//import squatImage from '../image/squat.png';
-//import plusButton from '../image/plus-button.png';
+import squatImage from '../image/squat.png';
+import plusButton from '../image/plus-button.png';
+import background from '../image/running.jpg';
 import { Link } from 'react-router-dom';
 
+import 'react-slideshow-image/dist/styles.css';
+import {  Fade } from 'react-slideshow-image'; //Fade , Zoom,Slide
+
 import "../CSS/Main.css";
+
+const slideImages = [
+    {
+      url: 'https://images.unsplash.com/photo-1427384906349-30452365b5e8?dpr=2&auto=compress,format&fit=crop&w=1199&h=899&q=80&cs=tinysrgb&crop=',
+      caption: 'Slide 1'
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+      caption: 'Slide 2'
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+      caption: 'Slide 3'
+    },
+  ];
+
+const divStyle ={
+    display:'flex',
+    alignItems: "center",
+    justifyContent : "center",
+    height : "100px",
+    backgroundSize: "100% 100px", // 배경 이미지를 요소에 맞춰서 축소 또는 확대하여 모두 보이도록 설정  
+}  
+
 
 const Home = () => {
     var token = window.localStorage.getItem('token');
@@ -44,6 +72,21 @@ const Home = () => {
             </div>   
         
         <div className='startAnimation'>
+
+            
+                <div className='slide-container' style={{width:'90%',borderRadius:'25px',marginLeft : "5%",marginBottom :"-30%",marginTop:"10%"}}>
+                    <Fade>
+                        {slideImages.map((image, index) =>(
+                            <div key={index}>
+                                <div style={{...divStyle, backgroundImage:`url(${image.url})` }}>
+                                    
+                                </div>
+                            </div>
+                        ))}
+                    </Fade>
+                </div>
+            
+            
                 
             <Link to="/challengemain">
                 <button className='btn primaryButton-outset'
