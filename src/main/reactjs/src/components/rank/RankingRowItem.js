8855e517016ea5deb1f36621e1d3904e3effb1ec
xiexiehanoi <ksw593vn@gmail.com>
2024-02-27 17:18:52 +0900

    [add] : ranking 변동사항 기능 추가(scheduler 6시간마다 갱신) 및 runawayQR코드(main접속가능) QR로 교체

--- src/main/reactjs/src/components/rank/RankingRowItem.js
+++ src/main/reactjs/src/components/rank/RankingRowItem.js
@@ -1,31 +1,41 @@
 import React from 'react';
-import goldmedal from "../../image/1st 1.png";
-import silvermedal from "../../image/2nd 1.png";
-import bronzemedal from "../../image/3rd 1.png";
+import goldMedal from "../../image/1st 1.png";
+import silverMedal from "../../image/2nd 1.png";
+import bronzeMedal from "../../image/3rd 1.png";
+import upArrow from "../../image/up-arrow.png";
+import downArrow from "../../image/down-arrow.png";
 
 const RankingRowItem = (props) => {
     const {row, idx} = props;
 
+    console.log("row:"+row.rankChange)
     return (
-        <li key={row.id} >
-        
-            <mark>{idx + 1 === 1 ? (
-            <img className="gold-medal" src={goldmedal} alt="gold medal"
-            style={{ width: '20%', height: 'auto' }}/>
-        ) : 
-         idx + 1 ===2 ? (
-            <img className="silver-medal" src={silvermedal} alt="silver medal"
-            style={{ width: '20%', height: 'auto' }}/>
-         ) :
-         idx + 1 ===3 ? (
-            <img className="bronze-medal" src={bronzemedal} alt="bronze medal"
-            style={{ width: '20%', height: 'auto' }}/>
-         ) :
-        
-        (
-            idx + 1
-        )} {row.nickname}</mark>            
-            <small>{row.point}</small>
+        <li key={row.id} className='ranking-list-body'>
+            <mark>
+                {idx + 1 === 1 ? (
+                    <img className="gold-medal" src={goldMedal} alt="gold medal" style={{ width: '20%', height: 'auto' }} />
+                ) : idx + 1 === 2 ? (
+                    <img className="silver-medal" src={silverMedal} alt="silver medal" style={{ width: '20%', height: 'auto' }} />
+                ) : idx + 1 === 3 ? (
+                    <img className="bronze-medal" src={bronzeMedal} alt="bronze medal" style={{ width: '20%', height: 'auto' }} />
+                ) : (
+                            idx + 1
+                        )} {row.nickname} ({row.point} 점)
+            </mark>
+            <div>
+                 {row.rankChange !== 0 ? (
+                    <div style={{display: 'flex', alignItems: 'center'}}>
+                        {row.rankChange > 0 ? (
+                            <img src={downArrow} alt="up arrow" style={{ width: '20px', height: 'auto' }} />
+                        ) : (
+                                    <img src={upArrow} alt="down arrow" style={{ width: '20px', height: 'auto' }} />
+                                )}
+                        <span>{Math.abs(row.rankChange)}</span>
+                    </div>
+                ) : (
+                            "-"
+                        )}
+            </div>
         </li>
     );
 };
