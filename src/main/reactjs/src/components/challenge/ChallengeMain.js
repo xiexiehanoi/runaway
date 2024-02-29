36d705d4bc67b0696fcd179db22fd7cb5a076cc3
xiexiehanoi <ksw593vn@gmail.com>
2024-02-29 21:09:57 +0900

    [fix] : common CSS로 수정

--- src/main/reactjs/src/components/challenge/ChallengeMain.js
+++ src/main/reactjs/src/components/challenge/ChallengeMain.js
@@ -37,14 +37,14 @@ const ChallengeMain = () => {
   }, []);
 
   return (
-    <div style={{ height: "100vh" }}>
+    <div className="CommonContainer" style={{ height: "100vh" }}>
       <div className="header-inscreen" style={{ padding: "10px" }}>
         챌린지 도전하기
       </div>
-      <section style={{ marginBottom: "16px" }}>
-        <div className="exercise-container">
-          <div className="exercise-item">
-            <div className="exercise-content">
+      <section style={{marginBottom: "16px" }}>
+        <div className="exercise-main-container exercise-button primaryCard">
+          <div className="exercise-item" style={{padding:"5px"}}>
+            <div className="exercise-content primaryCard" style={{marginBottom:"8px"}}>
               <Link to="/exercisechallenge">
                 <img
                   src={exerciseImg}
@@ -57,14 +57,14 @@ const ChallengeMain = () => {
             </div>
             <span>맨몸운동</span>
           </div>
-          <div className="exercise-item">
-            <div className="exercise-content">
+          <div className="exercise-item" style={{padding:"5px"}}>
+            <div className="exercise-content primaryCard" style={{marginBottom:"8px"}}>
               <Link to="/runningchallenge">
                 <img
                   src={runningImg}
                   alt="Running Challenge"
                   className="exercise-image"
-                  style={{ width: "80%", height: "80%" }}
+                  style={{ width: "100%", height: "100%" }}
                 />
                 <br />
               </Link>
@@ -74,7 +74,7 @@ const ChallengeMain = () => {
         </div>
       </section>
       <section>
-        <div className="header-inscreen" style={{ padding: "10px" }}>
+        <div className="subject1 challenge-list-title" style={{fontSize:"1.5em"}}>
           진행중인 챌린지 목록
         </div>
         <div>
